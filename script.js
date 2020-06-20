102dea22e1e7512de8140db6b1499c3d71b4092f
Kamila Duda <kakus86@o2.pl>
2020-06-20 18:17:54 +0200

    poprawki

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 let pig = document.querySelector(".pig");
 let startGame = false;
-const easyButton = document.querySelector(".easy__button");
-const mediumButton = document.querySelector(".medium__button");
-const hardButton = document.querySelector(".hard__button");
+const easyButton = document.querySelector(".game__easyButton");
+const mediumButton = document.querySelector(".game__mediumButton");
+const hardButton = document.querySelector(".game__hardButton");
 let coinsContainer = document.getElementById("coins__container");
 let message = "";
 let gameInfo = document.querySelector(".gameInfo");
-let command = document.querySelector(".command");
+let command = document.querySelector(".game__command");
 let bubble = document.querySelector(".pig__bubble");
 let image = coinsContainer.querySelectorAll("img");
-const checkButton = document.querySelector(".check__button");
-const amountContainer = document.querySelector(".amount__container");
-const promptButton = document.querySelector(".prompt__button");
+const checkButton = document.querySelector(".game__checkButton");
+const amountContainer = document.querySelector(".game__amount");
+const promptButton = document.querySelector(".pig__promptButton");
 let easyTile = document.querySelectorAll(".easy");
 let mediumTile = document.querySelectorAll(".medium");
 let hardTile = document.querySelectorAll(".hard");
@@ -41,19 +41,24 @@ function round(n, k) {
 //sprawdzam czy ekran jest dotykowy
 if (window.matchMedia("(pointer: coarse)").matches) {
     pig.insertAdjacentElement("afterend", document.querySelector(".game__level"));
-    document.body.addEventListener('touchmove', function (event) {
-        event.preventDefault();
-    }, false);
+    // document.body.addEventListener('touchmove', function (event) {
+    //     event.preventDefault();
+    // }, false);
 
     let money = document.querySelectorAll(".coin");
     money.forEach(function (oneCoin) {
-        oneCoin.addEventListener("touchstart", function (ev) {
-            ev.preventDefault();
-            oneCoin.style.position="fixed";
-            pig.classList.add("shadow");
-            pig.style.borderStyle = "dotted";
-            pig.classList.add("alternativeText");
+            oneCoin.addEventListener("touchstart", function (ev) {
+            if(ev.cancelable) {
+                ev.preventDefault();
+                oneCoin.style.position="fixed";
+                oneCoin.style.width="9vw";
+                oneCoin.style.height="9vw";
+                pig.classList.add("shadow");
+                pig.style.borderStyle = "dotted";
+                pig.classList.add("alternativeText");
+           }
         });
+        oneCoin.addEventListener("touchcancel", function (ev) {alert(ev);}, false);
     });
     money.forEach(function (oneCoin) {
         oneCoin.addEventListener("touchmove", function (ev) {
@@ -79,8 +84,11 @@ if (window.matchMedia("(pointer: coarse)").matches) {
                 oneCoin.style.position="relative";
                 oneCoin.style.left="unset";
                 oneCoin.style.top="unset";
+                oneCoin.style.width="5vw";
+                oneCoin.style.height="5vw";
                 pig.appendChild(oneCoin);
             }
+            return true;
         });
     });
 
@@ -112,6 +120,8 @@ if (window.matchMedia("(pointer: coarse)").matches) {
                     image.forEach(function (img) {
                         img.style.left = "unset";
                         img.style.top = "unset";
+                        img.style.width="9vw";
+                        img.style.height="9vw";
                         let imgValue = img.getAttribute('value');
                         let target = "";
                         if (imgValue == 0.01) {
@@ -276,7 +286,7 @@ if (window.matchMedia("(pointer: coarse)").matches) {
 }
 
 promptButton.addEventListener("click", () => {
-    let amount = parseFloat(document.querySelector(".amount__container").innerHTML);
+    let amount = parseFloat(document.querySelector(".game__amount").innerHTML);
     let promptArray = [];
     let posibbleMoney = [5, 2, 1, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01];
     let remain = amount;
@@ -348,7 +358,7 @@ hardButton.addEventListener("click", function () {
     x = hardArray;
     gameInfo.style.display = "none";
     checkButton.style.opacity = "1";
-    command.style.opacity = "1";
+    command.style.opacity = "1";    
     drawTheAmount(hardArray);
     easyTile.forEach(function (easyBlock) {
         easyBlock.classList.add("easy");
